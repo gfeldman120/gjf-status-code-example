00cd39269e093c08cea67d6ba9d154c2add3c784
Gavin Feldman <gjf2751@rit.edu>
2026-09-11 14:41:48 -0400

    Base exercise

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -21,6 +21,13 @@ const badRequest = (request, response) => {
     message: 'This request has the required parameters',
   };
 
+  if(!request.query.valid || request.query.valid !== true) {
+    responseJSON.message = "Missing valid query params set to true";
+    responseJSON.id = 'badRequest';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const notFound = (request, response) => {
@@ -29,6 +36,7 @@ const notFound = (request, response) => {
     id: 'notFound',
   };
 
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 module.exports = {
